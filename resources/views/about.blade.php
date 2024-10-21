5b427b49c176e8fad002661c995b839caeca3494
Opjasson <azmitheusos333@gmail.com>
2024-10-21 20:07:05 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Halaman about</title>
+</head>
+<body>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+    </ul>
+   <h1>ini adalah halaman about</h1>
+   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum amet perferendis animi dignissimos nesciunt nemo repudiandae. Fuga, nesciunt! Distinctio, voluptatem!</p>
+
+   <script src="./js/script.js"></script>
+</body>
+</html>
