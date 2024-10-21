e867e20308dd09b6973e96f3cf07dfcd6b77b76a
Opjasson <azmitheusos333@gmail.com>
2024-10-21 20:29:27 +0700

    struktur program, eps3

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -13,8 +13,9 @@
         <li><a href="/about">About</a></li>
     </ul>
    <h1>ini adalah halaman about</h1>
+   <h3>Hallo nama saya : {{ $nama }}</h3>
    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum amet perferendis animi dignissimos nesciunt nemo repudiandae. Fuga, nesciunt! Distinctio, voluptatem!</p>
-
+    <img src="./img/menu2.jpg" alt="menu">
    <script src="./js/script.js"></script>
 </body>
 </html>
