ab2c99e11a6691a888c83fd54c20f51f2d343b4a
Opjasson <azmitheusos333@gmail.com>
2024-10-22 07:25:18 +0700

    add tailwindcss

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,6 +11,8 @@
     <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
+        <li><a href="/blog">Blog</a></li>
+        <li><a href="/contact">Contact</a></li>
     </ul>
    <h1>ini adalah halaman about</h1>
    <h3>Hallo nama saya : {{ $nama }}</h3>
